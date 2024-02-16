b52663055f6126973209c29104d50f024384b3bf
AliBabaei-i <155062135+AliBabaei-i@users.noreply.github.com>
2024-02-16 10:27:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -44,20 +44,9 @@ const input = document.querySelector("#input");
 const submit = document.querySelector("#btn");
 
 input.addEventListener("input", (e) => {
-    document.querySelector("#error").style.visibility = "hidden";
-    if (e.target.value.length > 18) {
-        input.value = `${e.target.value.substring(0, 16)}...`;
-    } else {
-        return;
-    }
     e.preventDefault();
 });
 input.addEventListener("click", (e) => {
-    if (e.target.value.length > 18) {
-        input.value = `${e.target.value.substring(0, 16)}...`;
-    } else {
-        return;
-    }
     e.preventDefault();
 });
 document.querySelector("#form").addEventListener("submit", (e) => {
@@ -67,8 +56,7 @@ submit.addEventListener("click", (e) => {
     e.preventDefault();
 });
 
-const notFound = (r) => {
-    console.log("notFound running" + r);
+const notFound = () => {
     document.querySelector("#error").style.visibility = "visible";
     document.querySelector("#username").innerHTML = "@octocat";
     document.querySelector("#name").innerHTML = `The Octocat`;
@@ -80,125 +68,22 @@ const notFound = (r) => {
     document.querySelector("#repo").innerHTML = "0";
     document.querySelector("#input").placeholder = "";
 };
-// const setDate = (data) => {
-//     const date = data.created_at;
-//     const dateArray = date.split("-", 4);
-//     if (dateArray[1] == "01") {
-//         var month = "Jan";
-//     }
-//     if (dateArray[1] == "02") {
-//         var month = "Feb";
-//     }
-//     if (dateArray[1] == "03") {
-//         var month = "Mar";
-//     }
-//     if (dateArray[1] == "04") {
-//         var month = "Apr";
-//     }
-//     if (dateArray[1] == "05") {
-//         var month = "May";
-//     }
-//     if (dateArray[1] == "06") {
-//         var month = "Jun";
-//     }
-//     if (dateArray[1] == "07") {
-//         var month = "Jul";
-//     }
-//     if (dateArray[1] == "08") {
-//         var month = "Aug";
-//     }
-//     if (dateArray[1] == "09") {
-//         var month = "Sep";
-//     }
-//     if (dateArray[1] == "10") {
-//         var month = "Jan";
-//     }
-//     if (dateArray[1] == "11") {
-//         var month = "Oct";
-//     }
-//     if (dateArray[1] == "12") {
-//         var month = "Nov";
-//     }
-//     document.querySelector(
-//         "#join-date"
-//     ).innerHTML = `Joined ${month} ${dateArray[0]}`;
-// };
-// const contact = (data) => {
-//     if (data.location == null || data.location == "") {
-//         document.querySelector("#location").innerHTML = "Not Available";
-//         document.querySelector("#location").style.opacity = "0.5";
-//         document.querySelector("#location-img").style.opacity = "0.5";
-//     } else {
-//         document.querySelector("#location").innerHTML = `${data.location}`;
-//         document.querySelector("#location").style.opacity = "1";
-//         document.querySelector("#location-img").style.opacity = "1";
-//     }
-//     if (data.twitter_username == null || data.twitter_username == "") {
-//         document.querySelector("#twitter").innerHTML = "Not Available";
-//         document.querySelector("#twitter-img").style.opacity = "0.5";
-//         document.querySelector("#twitter").style.opacity = "0.5";
-//     } else {
-//         document.querySelector("#twitter").innerHTML = data.twitter_username;
-//         document.querySelector("#twitter").style.opacity = "1";
-//         document.querySelector("#twitter").style.color = "var(--Dark-Gray)";
-//         document.querySelector("#twitter-img").style.color = "var(--Dark-Gray)";
-//         document.querySelector("#twitter-img").style.opacity = "1";
-//     }
-//     if (data.blog == null || data.blog == "") {
-//         document.querySelector("#awebsite").innerHTML = "Not Available";
-//         document.querySelector("#awebsite-img").style.opacity = "0.5";
-//         document.querySelector("#awebsite").style.opacity = "0.5";
-//     } else {
-//         document.querySelector("#awebsite").innerHTML = data.blog;
-//         document.querySelector("#awebsite").src = data.blog;
-//         document.querySelector("#awebsite").style.opacity = "1";
-//         document.querySelector("#awebsite").style.color = "var(--Dark-Gray)";
-//         document.querySelector("#awebsite-img").style.color =
-//             "var(--Dark-Gray)";
-//         document.querySelector("#awebsite-img").style.opacity = "1";
-//     }
-//     document.querySelector("#acompany").innerHTML = data.company;
-//     document.querySelector("#awebsite").src = data.html_url;
-// };
-// function setData(data) {
-//     setDate(data);
-//     contact(data);
-//     document.querySelector("#name").innerHTML = `The ${data.login}`;
-//     document.querySelector("#username").innerHTML = `@${data.login}`;
-//     if (data.bio == "" || data.bio == null) {
-//         document.querySelector("#bio").innerHTML = "This user has no bio.";
-//     }
-//     document.querySelector("#bio").innerHTML = data.bio;
-//     const char = new RegExp("[\u0600-\u06FF]");
-//     document.querySelector("#bio").style.direction =
-//         char.test(document.querySelector("#bio").value) === true
-//             ? "rtl"
-//             : "ltr";
-//     document.querySelector("#followers").innerHTML = data.following;
-//     document.querySelector("#following").innerHTML = data.followers;
-//     document.querySelector("#repo").innerHTML = data.public_repos;
-//     document.querySelector("#avatar").src = data.avatar_url;
-// }
 async function getUser() {
-    // if (input.value == "") {
-    //     document.querySelector("#error").style.visibility = "visible";
-    //     document.querySelector("#error").innerHTML = "Enter username";
-    // }
     await fetch(url + input.value)
         .then((response) => {
             if (!response.ok) {
                 if (response.status === 404) {
-                    notFound(1);
+                    notFound();
                     throw new Error("Data not found");
                 } else if (response.status === 500) {
-                    notFound(2);
+                    notFound();
                     throw new Error("Server error");
                 } else if (response.status === 403) {
-                    notFound(3);
+                    notFound();
                     alert("need vpn to load data");
                     throw new Error("need vpn to load data");
                 } else {
-                    notFound(4);
+                    notFound();
                     throw new Error("Network response was not ok");
                 }
             }
@@ -213,15 +98,6 @@ async function getUser() {
                 document.querySelector("#bio").innerHTML =
                     "This user has no bio.";
             }
-            // document.querySelector("#bio").style.direction =
-            //     data.bio.split("ی").length - 1 > data.bio.split("a").length - 1
-            //         ? "rtl"
-            //         : "ltr";
-            // const char = new RegExp("[\u0600-\u06FF]");
-            // document.querySelector("#bio").style.direction =
-            //     char.test(document.querySelector("#bio").value) === true
-            //         ? "rtl"
-            //         : "ltr";
             document.querySelector("#followers").innerHTML = data.following;
             document.querySelector("#following").innerHTML = data.followers;
             document.querySelector("#repo").innerHTML = data.public_repos;
@@ -318,23 +194,8 @@ async function getUser() {
                 document.querySelector("#awebsite").innerHTML = data.blog;
                 const web = document.querySelector("#website");
                 web.querySelector("div.items.website a").href = data.blog;
-                // web.querySelector("div.items.website a").style.opacity = "0.5";
                 document.querySelector("#awebsite").style.opacity = "1";
-                // document.querySelector("#awebsite").style.color =
-                //     "var(--Dark-Gray)";
-                // document.querySelector("#awebsite-img").style.color =
-                //     "var(--Dark-Gray)";
                 document.querySelector("#website-img").style.opacity = "1";
             }
         });
-    // .catch((error) => {
-    //     notFound(5);
-    //     console.error("Error", error);
-    // });
 }
-
-// document.querySelector("#input").addEventListener("keydown", (e) => {
-//     if (e.code === "Enter") {
-//         getUser();
-//     }
-// });
